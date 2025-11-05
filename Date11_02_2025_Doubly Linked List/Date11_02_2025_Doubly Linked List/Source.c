9beab1ce56a66d2a40d517a294ac00bb03ddcb42
Wed20190109 <Wed20190109@outlook.com>
2025-11-05 02:57:01 -0800

    Add josephus problem.

--- Date11_02_2025_Doubly Linked List/Date11_02_2025_Doubly Linked List/Source.c
+++ Date11_02_2025_Doubly Linked List/Date11_02_2025_Doubly Linked List/Source.c
@@ -1,4 +1,4 @@
-#include "DoublyLinkedList.h"
+﻿#include "DoublyLinkedList.h"
 void SimpleTest()
 {
 	node *a=NULL;
@@ -32,5 +32,68 @@ void SimpleTest()
 int main()
 {
 	SimpleTest();
+
+
+	
 	return 0;
 }
+
+
+/*牛客网约瑟夫环问题代码
+typedef struct Node{
+int val;
+ struct Node*next;
+}node;
+
+node * createnode(int x)
+{
+node * newnode=(node*)malloc( sizeof(node));
+if(newnode==NULL)
+{
+	perror("malloc");
+	exit(0);
+}
+newnode->val=x;
+newnode->next=NULL;
+return newnode;
+}
+
+node *createcircle(int n)
+{
+	node * head=createnode(1);
+	node*tail=head;
+	for(int i=2;i<=n;i++)
+	{
+		tail->next=createnode(i);
+		tail=tail->next;
+	}
+	tail->next=head;
+	return tail;
+}
+
+int ysf(int n, int m ) {
+	// write code here
+
+
+node*prev=createcircle(n);
+node *cur=prev->next;
+int count=1;
+while(cur->next!=cur)
+{
+	if(count==m)
+	{
+	   prev->next=cur->next;
+		free(cur);
+	 cur=prev->next;
+	 count=1;
+	}
+	else
+	{
+prev=cur;
+cur=cur->next;
+count++;
+	}
+}
+return cur->val;
+}
+*/
